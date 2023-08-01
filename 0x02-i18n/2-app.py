9719c72ca303969f485627cd2326622ca526b187
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-01 22:42:40 +0100

    Annotate fucntion

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
+class Config(object):
     """Configure the flask application"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -18,17 +18,15 @@ class Config:
 app.config.from_object('1-app.Config')
 
 
-def get_locale():
-    """Determine the best match with our supported languages"""
-    return requests.accept_language.best_match(app.Config['LANGUAGES'])
-
-
 @app.route('/', strict_slashes=False)
 def index():
     """Return index.html page"""
 
     return render_template('2-index.html')
 
+def get_locale() -> str:
+    """Determine the best match with our supported languages"""
+    return requests.accept_language.best_match(app.Config['LANGUAGES'])
 
 if __name__ == "__main__":
     app.run(debug=True)
