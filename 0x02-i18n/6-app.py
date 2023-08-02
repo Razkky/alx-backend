7e9ac1d8b254d4b1aae091e1a83a904955d34a99
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-02 16:35:51 +0100

    check for locale parameter in url

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -40,6 +40,10 @@ def get_locale() -> str:
             locale = user['locale']
             if locale in app.config['LANGUAGES']:
                 return locale
+    if request.args.get('locale'):
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
