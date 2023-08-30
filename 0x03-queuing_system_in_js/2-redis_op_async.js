939c275a4da841962326e45e7ea9dd11d213660d
Abdul-Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-30 22:00:20 +0100

    add and retrieve key from redis server

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+const { promisify } = require('util');
+
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const value = await getAsync(schoolName);
+  console.log(value);
+}
+
+async function main() {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
+
+main()
